fdbabafb2aa19dd587d553eb86c1a990d0fe7c14
Satwik Baramal <satwikbaramal854@gmail.com>
2024-08-05 01:33:55 +0530

    MadetheFrontEnd

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let a=10;
+let b=20;
+let c=0;
+
+function add(a,b){
+    return a+b;
+};
+
+function sub(a,b){
+    return a-b;
+};
+
+function mul(a,b){
+    return a*b;
+};
+
+function division(a,b){
+    return a/b;
+};
+
+// console.log(add(a,b));
+// console.log(sub(a,b));
+// console.log(mul(a,b));
+// console.log(division(a,b));
+
+// c=add(a,b);
+// console.log(c);
